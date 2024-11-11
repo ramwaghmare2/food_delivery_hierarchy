c8738bdd637dd0cde3fdf7108ff68ea89247a8c3
ramwaghmare2 <ramwaghmare287@gmail.com>
2024-11-11 16:15:01 +0530

    Admin Panel

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db
-from routes import create_app_routes
+from routes import create_app_routes  # Import create_app_routes from routes
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
+    app.secret_key = "asf18fsf8s14fsafsdf48sd"
 
     db.init_app(app)
 
     # Initialize Flask-Migrate
     migrate = Migrate(app, db)
 
-    # Register routes
-    create_app_routes(app)
+    # Register all blueprints through create_app_routes
+    create_app_routes(app)  # This will register admin_bp and other blueprints
 
     return app
 
