8dc31791568de8e1446a6b8ab72eeae131d76820
rohit <tiwarir928@gmail.com>
2024-11-11 17:08:59 +0530

    Super Distributor and Distributor routes

--- hierarchy/Scripts/activate.bat
+++ hierarchy/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\Desktop\food_delivery_hierarchy\hierarchy
+set VIRTUAL_ENV=C:\Users\tiwar\OneDrive\Desktop\Open Future Technologies\food_delivery_hierarchy\hierarchy
 
 if not defined PROMPT set PROMPT=$P$G
 
